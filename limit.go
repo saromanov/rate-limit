f945c0b30dd904e8182f3f6ae7233b2dd4528f34
Sergey <xxsmotur@gmail.com>
2019-02-09 01:05:02 +0500

    first draft

--- limit.go
+++ limit.go
@@ -1,6 +1,7 @@
 package limit
 
 import (
+	"errors"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -36,18 +37,15 @@ func (r *Limiter) Do() {
 func (r *Limiter) apply() (time.Duration, error) {
 	r.m.Lock()
 	defer r.m.Unlock()
-	now := time.Now()
+	//now := time.Now()
 	var cr uint32
 	c := atomic.LoadUint32(&cr)
 	if l := c; l < r.limit {
-		atomic.AddUint32(&r.couner, 1)
-		return true, 0
+		atomic.AddUint32(&r.counter, 1)
+		return 0, nil
 	}
-	frnt := r.times.Front()
-	if diff := now.Sub(frnt.Value.(time.Time)); diff < r.interval {
-		return false, r.interval - diff
+	if time.Second < r.interval {
+		return r.interval - time.Second, errors.New("closed interval")
 	}
-	frnt.Value = now
-	r.times.MoveToBack(frnt)
-	return true, 0
+	return 0, nil
 }
